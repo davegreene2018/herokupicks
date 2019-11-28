809eb562a4e7dd2b7738ba50aa2105de9a35cfa3
davegreene2018 <davegreene2017@gmail.com>
2019-11-28 16:37:01 +0000

    heroku

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,83 @@
+class StaticPagesController < ApplicationController
+  def home
+    @orders = Order.all
+    @orderitems = Orderitem.all
+    @items = Item.all
+    
+    @categories = Category.all
+  end
+  
+  def fender
+       @items = Item.where("brand like ? ", fender)
+  end
+  
+  def gibson
+      
+  end
+  
+  def taylor
+      
+  end
+  
+  def takamine 
+      
+  end
+  
+  def martin
+      
+  end
+
+  
+  
+  def category
+    catName = params[:title]
+    @items = Item.where("category like ? ", catName)
+  end
+
+
+  def help
+  end
+
+  def about
+  end
+  
+  def createOrder
+ 
+   @orders = Order.all
+          
+  end
+  
+  def users
+   
+    @users = User.all
+  end
+  
+  
+  
+  def upgrade
+        @user.update_attribute(:adminrole, true)
+        redirect_to :action => :admin_users
+  end
+    
+    def downgrade_admin
+       @user.update_attribute(:adminrole, false)
+         redirect_to :action => :admin_users
+    end   
+
+  
+  def paid
+    # redirect_to "/cart/clear"
+    flash[:notice] = 'Transaction Complete'
+    @order = Order.last
+    @order.update_attribute(:status , "Paid by User: #{current_user.email}")
+    #"Paid by User:#{current_user.id} #{current_user.name} #{current_user.surname}")
+    session[:cart] = nil
+    
+    @orders = Order.all
+    @orderitems = Orderitem.all
+    @items = Item.all
+    
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,67 @@
+Rails.application.routes.draw do
+  
+  resources :wishlists
+  resources :categories
+  resources :orders do 
+    resources:orderitems
+  end
+
+  devise_for :users do
+    resources :orders 
+  end
+  get 'cart/index'
+
+  resources :items
+  root :to => 'static_pages#home'
+
+  get '/help' => 'static_pages#help'
+
+  get '/about' => 'static_pages#about'
+  
+  get '/login' => 'user#login'
+  
+  get '/logout' => 'user#logout'
+  
+  get '/cart', to: 'cart#index'
+  
+  get '/cart/:id', to: 'cart#add'
+  
+  get '/clearcart', to: 'cart#clearCart'
+  
+  get '/cart/remove/:id' => 'cart#remove'
+  
+  get '/cart/decrease/:id' => 'cart#decrease'
+  
+  get '/cart/increase/:id' => 'cart#increase'
+  
+  get '/checkout' => 'cart#createOrder'
+  
+  get '/paid/:id' => 'static_pages#paid'
+  
+  get '/users' => 'static_pages#users'
+  
+  get '/upgrade/:id' => 'static_pages#upgrade'
+  
+  get '/upgrade/:id' => 'static_pages#downgrade'
+  
+  post '/search' => 'items#search'
+  
+  get '/search' => 'static_pages#searchproducts'
+  
+  get 'category/:title', to: 'static_pages#category'
+  
+  get '/bag/:id', to: 'cart#bag'
+  
+  get '/fender' => 'static_pages#fender'
+  get '/gibson' => 'static_pages#gibson'
+  get '/taylor' => 'static_pages#taylor'
+  get '/martin' => 'static_pages#martin'
+  get '/takamine' => 'static_pages#takamine'
+  
+  
+  
+  
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
